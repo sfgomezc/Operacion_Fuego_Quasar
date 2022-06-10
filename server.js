05b7cd072ed60ced5fe5d1c50a78f694a4fb7b35
Steven Gomez <steven.gomez.c@gmail.com>
2022-06-10 09:53:08 -0500

    Agregar server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+// Modules
+const express = require('express');
+const morgan = require('morgan');
+
+// Config
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 5000);
+app.set('json spaces', 2);
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// Routes
+app.use('/api/v1', require('./routes/index'))
+
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port: ${app.get('port')}.`);
+});
