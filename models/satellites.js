f37bdfbeb0f92c42bce3613b25fe600eec54e6ad
Steven Gomez <steven.gomez.c@gmail.com>
2022-06-13 22:23:20 -0500

    Agregar datos de satelites

---
+++ models/satellites.js
@@ -0,0 +1,26 @@
+//Satellites position currently in service
+let satellites = [
+    {
+        name: 'kenobi',
+        position: {
+            x: -500,
+            y: -200
+        }
+    },
+    {
+        name: 'skywalker',
+        position: {
+            x: 100,
+            y: -100
+        }
+    },
+    {
+        name: 'sato',
+        position: {
+            x: 500,
+            y: 100
+        }
+    }
+];
+
+exports.satellites = satellites;
